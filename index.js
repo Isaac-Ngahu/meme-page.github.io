cba8b955cfa8f0f3909adb1552672d2364388127
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-04-25 23:24:31 +0300

    adding navigation pane

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const like_button=document.getElementById("like-button")
+const meme_holder=document.getElementById("meme-holder")
 like_button.addEventListener("click",handleClick)
 function handleClick(){
     if(like_button.style.color==="grey"){
@@ -7,3 +8,13 @@ function handleClick(){
         like_button.style.color="grey"
     }
 }
+function getMemeData(){
+    fetch("http://alpha-meme-maker.herokuapp.com")
+    .then(res=>res.json())
+    .then(data=>replaceWithData(data))
+}
+function replaceWithData(data){
+    meme_holder.src=data.url
+
+}
+getMemeData()
