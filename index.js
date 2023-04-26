7bcdfe66156017a790fc68389a628d216b15ba6f
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-04-26 14:15:25 +0300

    activating the generate meme button

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const like_button=document.getElementById("like-button")
 const meme_holder=document.getElementById("meme-holder")
-like_button.addEventListener("click",handleClick)
+
 const meme_title=document.getElementById("meme-title")
 const author_name=document.getElementById("author-name")
+const meme_button=document.getElementById("meme-button")
 function handleClick(){
     if(like_button.style.color==="grey"){
         like_button.style.color="red"
@@ -11,15 +12,18 @@ function handleClick(){
     }
 }
 function getMemeData(){
-    fetch("https://meme-api.com/gimme/60")
+    fetch("https://meme-api.com/gimme/30")
     .then(res=>res.json())
     .then(data=>replaceWithData(data))
 }
 function replaceWithData(data){
    data.memes.forEach(element => {
-    author_name.innerText=element.author;
-    meme_title.innerText=element.title;
+    author_name.innerText=` The author is:${element.author}`;
+    meme_title.innerText=`The title is:${element.title}`;
     meme_holder.src=element.url;
+
    });
 }
 getMemeData()
+like_button.addEventListener("click",handleClick)
+meme_button.addEventListener("click",getMemeData)
