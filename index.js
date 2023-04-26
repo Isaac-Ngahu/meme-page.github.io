91fa2d127b2c5428f75a723410e237ae34a3f89b
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-04-26 16:31:22 +0300

    designing submit buttons

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const subreddit=document.getElementById("subreddit")
 const meme_title=document.getElementById("meme-title")
 const author_name=document.getElementById("author-name")
 const meme_button=document.getElementById("meme-button")
-const postLink=document.getElementById("postlink")
+
 function handleClick(){
     if(like_button.style.color==="grey"){
         like_button.style.color="red"
@@ -16,15 +16,19 @@ function getMemeData(){
     fetch("https://meme-api.com/gimme/30")
     .then(res=>res.json())
     .then(data=>replaceWithData(data))
+    .catch(error=>{
+        alert("oops looks like there was an error");
+        console.log(error)
+    })
 }
 function replaceWithData(data){
     like_button.style.color="grey"
    data.memes.forEach(element => {
      meme_title.innerText=`The title is:${element.title}`;
     meme_holder.src=element.url;
-    author_name.innerText=` The author is:${element.author}`;
-subreddit.innerText=`The subreddit is${element.subreddit}`
-postLink.innerText=`click link to get meme${element.postLink}`
+    author_name.innerHTML=` The author is:<u>${element.author}</u>`;
+subreddit.innerHTML=`The subreddit is<u>${element.subreddit}</u>`
+
    });
 }
 getMemeData()
