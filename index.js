7c14aabaf15a929f92377b191f3e4c587d03198b
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-04-26 16:04:42 +0300

    adding meme details

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const like_button=document.getElementById("like-button")
 const meme_holder=document.getElementById("meme-holder")
-
+const subreddit=document.getElementById("subreddit")
 const meme_title=document.getElementById("meme-title")
 const author_name=document.getElementById("author-name")
 const meme_button=document.getElementById("meme-button")
+const postLink=document.getElementById("postlink")
 function handleClick(){
     if(like_button.style.color==="grey"){
         like_button.style.color="red"
@@ -17,11 +18,13 @@ function getMemeData(){
     .then(data=>replaceWithData(data))
 }
 function replaceWithData(data){
+    like_button.style.color="grey"
    data.memes.forEach(element => {
-    author_name.innerText=` The author is:${element.author}`;
-    meme_title.innerText=`The title is:${element.title}`;
+     meme_title.innerText=`The title is:${element.title}`;
     meme_holder.src=element.url;
-
+    author_name.innerText=` The author is:${element.author}`;
+subreddit.innerText=`The subreddit is${element.subreddit}`
+postLink.innerText=`click link to get meme${element.postLink}`
    });
 }
 getMemeData()
