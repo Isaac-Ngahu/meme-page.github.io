397d27c985332c63a0f95448d808f4db66ff0f84
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-04-26 14:03:45 +0300

    adding author names

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const like_button=document.getElementById("like-button")
 const meme_holder=document.getElementById("meme-holder")
 like_button.addEventListener("click",handleClick)
+const meme_title=document.getElementById("meme-title")
+const author_name=document.getElementById("author-name")
 function handleClick(){
     if(like_button.style.color==="grey"){
         like_button.style.color="red"
@@ -9,11 +11,15 @@ function handleClick(){
     }
 }
 function getMemeData(){
-    fetch("https://meme-api.com/gimme")
+    fetch("https://meme-api.com/gimme/60")
     .then(res=>res.json())
     .then(data=>replaceWithData(data))
 }
 function replaceWithData(data){
-    meme_holder.setAttribute("src",data.url)
+   data.memes.forEach(element => {
+    author_name.innerText=element.author;
+    meme_title.innerText=element.title;
+    meme_holder.src=element.url;
+   });
 }
 getMemeData()
